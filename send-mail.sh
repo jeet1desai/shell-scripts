52a3224743222b8c2e021599c6c6d4c54507160b
Jeet Desai <jeetdesai995@gmail.com>
2023-11-18 21:12:59 +0530

    Send mail script added

--- send-mail.sh
+++ send-mail.sh
@@ -9,4 +9,43 @@
 # Description: Send mail to person
 ###########################################################################
 
-set -x # Debug mode
+# set -x # Debug mode
+# set -e 
+
+# sender=""
+# receiver=""
+
+# read -p "Please enter subject: " subject
+# read -p "Please enter body: " body
+
+email_file="/home/jeet/Documents/shell-scripts/email.txt"
+cat $email_file
+
+read -p "Please attach file name (or leave balck if none): " file
+
+if [ -f "$file" -a -s "$file" ]
+then
+	echo "File Exist"
+	#Mime=$(file --mime-type "$file" | sed 's/.*: //')
+	#echo "$Mime"
+	#echo "$body" | mail -s "$subject" -a "Content-Type:$Mime" -A "$file" "$receiver"
+	sendmail -A "$file" -i -t < "$email_file" 2> ./error.log
+else
+	echo "File not exist"
+	sendmail -i -t < "$email_file" 2> ./error.log
+	#echo "$body" | mail -s "$subject" "$receiver"
+fi
+
+if [ $? -eq 0 ]
+then 
+	rm ./error.log
+	echo "Email sent successfully"
+else
+	echo "Something went wrong while sending email, Please check error.log file"
+fi
+
+
+
+
+
+
